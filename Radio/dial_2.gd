ac85920e51fe9e515ca33bf4250838000d8cf9cf
Waleed Aldawoud <waleed.s.d.09@gmail.com>
2026-06-20 23:27:33 +0300

    syncing voice#2

--- Radio/dial_2.gd
+++ Radio/dial_2.gd
@@ -4,10 +4,10 @@ extends Control
 
 @export var central_noise_channel:CentralAndStaticNoiseChannels 
 func _on_button_button_up() -> void:
-	var new_id:int =wrapi(central_noise_channel.currently_chosen_channel-1,0,central_noise_channel.amount_of_streams)
+	var new_id:int =wrapi(central_noise_channel.currently_chosen_channel+1,0,central_noise_channel.amount_of_streams)
 	%CentralAndStaticNoiseChannels.switch_channel(new_id)
 
 
 func _on_button_2_button_up() -> void:
-	var new_id:int =  wrapi(central_noise_channel.currently_chosen_channel+1,0,central_noise_channel.amount_of_streams)
+	var new_id:int =  wrapi(central_noise_channel.currently_chosen_channel-1,0,central_noise_channel.amount_of_streams)
 	%CentralAndStaticNoiseChannels.switch_channel(new_id)
