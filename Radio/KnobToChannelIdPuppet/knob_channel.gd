71b5c6944d5b4562324ea2226e693a2969f6a193
Waleed Aldawoud <waleed.s.d.09@gmail.com>
2026-06-22 21:12:03 +0300

    optimization for knob

--- Radio/KnobToChannelIdPuppet/knob_channel.gd
+++ Radio/KnobToChannelIdPuppet/knob_channel.gd
@@ -1,8 +1,10 @@
 extends KnobPuppet
 @export var central_and_static_noise_channels: CentralAndStaticNoiseChannels
-
+var last_channel := -1
 func on_rotation_change(angle_rad:float):
 	var angle_deg := fposmod(rad_to_deg(angle_rad), 360)
 	var channel = floor((angle_deg / 360.0) * central_and_static_noise_channels.amount_of_streams)
-	if not channel == central_and_static_noise_channels.currently_chosen_channel:
+	if not channel == last_channel:
+		last_channel = channel
 		central_and_static_noise_channels.switch_channel(channel)
+		print("channel_switch_pending...")
