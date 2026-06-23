3c17caf27b84a938f4dc0229a6bd1c77a8b1ffe5
Elliot Key <81060805+GreenBurningGK@users.noreply.github.com>
2026-06-23 09:23:13 -0700

    changed visuals for radio info

--- Radio/test/channel_id.gd
+++ Radio/test/channel_id.gd
@@ -8,4 +8,4 @@ func _ready() -> void:
 	central_and_static_noise_channels.new_channel_switched.connect(text_change)
 	
 func text_change():
-	text = str(central_and_static_noise_channels.currently_chosen_channel) + "." + str(int(round(knob_slider.value)))
+	text = str(central_and_static_noise_channels.currently_chosen_channel) + "." + str(int(round(knob_slider.value/3.63636)))
