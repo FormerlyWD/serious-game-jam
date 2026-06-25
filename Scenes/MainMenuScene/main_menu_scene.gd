38471a3eec5725e476f6274d42b9c99017ecf776
Waleed Aldawoud <waleed.s.d.09@gmail.com>
2026-06-25 12:59:50 +0300

    some modifications

--- Scenes/MainMenuScene/main_menu_scene.gd
+++ Scenes/MainMenuScene/main_menu_scene.gd
@@ -1,5 +1,7 @@
 extends Node2D
 @export var scene_fade_silhouette: SceneFadeSilhouette
-
+@export var default_bg_music:AudioStream
 func _ready() -> void:
+	$BgMusic.replace_music(default_bg_music)
+	
 	scene_fade_silhouette.fade_out()
