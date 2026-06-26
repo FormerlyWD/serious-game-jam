b8da8f5c19e575fd57043a88d1ce2ee16df909bb
Elliot Key <81060805+GreenBurningGK@users.noreply.github.com>
2026-06-25 18:03:07 -0700

    added art assets

--- Scenes/IntroPageScene/intro_page_scene.gd
+++ Scenes/IntroPageScene/intro_page_scene.gd
@@ -17,7 +17,7 @@ func _ready() -> void:
 		cur_tween = null
 	intro_page.position.y = 700
 	cur_tween = create_tween()
-	cur_tween.tween_property(intro_page,"position:y",0,duration)
+	cur_tween.tween_property(intro_page,"position:y",0,duration).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_EXPO)
 	$Button.disabled = false
 
 func _on_button_pressed() -> void:
@@ -27,6 +27,6 @@ func _on_button_pressed() -> void:
 		cur_tween = null
 	intro_page.position.y = 0
 	cur_tween = create_tween()
-	cur_tween.tween_property(intro_page,"position:y",700,duration)
+	cur_tween.tween_property(intro_page,"position:y",700,duration).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_EXPO)
 	await cur_tween.finished
 	GlobalShiftManager.next_shift()
