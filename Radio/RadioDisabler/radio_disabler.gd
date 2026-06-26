c3f96ae6ad184f82a3a69fd926716e555b7fc647
Waleed Aldawoud <waleed.s.d.09@gmail.com>
2026-06-26 09:08:07 +0300

    disable_functionality starter

--- Radio/RadioDisabler/radio_disabler.gd
+++ Radio/RadioDisabler/radio_disabler.gd
@@ -6,6 +6,12 @@ func disable_clicks():
 	for control_node in all_disables:
 		control_node.mouse_filter = Control.MOUSE_FILTER_IGNORE
 	
+func disable_functionality():
+	for control_node in all_disables:
+		control_node.disable_functionality()
+func enable_functionality():
+	for control_node in all_disables:
+		control_node.enable_functionality()
 func enable_clicks():
 	for control_node in all_disables:
 		control_node.mouse_filter = Control.MOUSE_FILTER_PASS
